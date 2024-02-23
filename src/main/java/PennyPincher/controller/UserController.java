ee40df3ee94a3b59ddaf28b58bdab153dad3bdc0
damia <damian.kwc@o2.pl>
2024-02-23 21:43:43 +0100

    Improved profile page.

--- src/main/java/PennyPincher/controller/UserController.java
+++ src/main/java/PennyPincher/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Data
@@ -50,8 +51,12 @@ public class UserController {
     public String userProfile(Model model) {
         User loggedInUser = userService.getCurrentlyLoggedInUser();
         List<Event> userEvents = loggedInUser.getUserEvents();
+
+        userEvents.sort(Comparator.comparing(Event::getEventBalance));
+
         model.addAttribute("userEvents", userEvents);
         model.addAttribute("loggedInUserName", loggedInUser.getUsername());
+        model.addAttribute("userBalance", loggedInUser.getBalance());
         return "profile";
     }
 
